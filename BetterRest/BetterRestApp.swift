1ee7cb641d2e244dafc9badcda91487990cb9af9
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2024-11-12 22:08:44 -0800

    Initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jasper Tan on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
