261b9fd88c8c41ed6d898d518dbc88e161c534ae
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2024-11-13 08:23:03 -0800

    Create initial UI, created sleep ML model

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,17 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            Form {
+                VStack {
+                    //MARK: Set Wakeup time
+                    Text("What time do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker("Enter a wakeup time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                
+                VStack {
+                    //MARK: Set Desired Sleep Amount
+                    Text("Desired Amount of Sleep")
+                        .font(.headline)
+                    
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12)
+                }
+                
+                VStack {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    
+                    Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+                }
+            }
+            .navigationTitle("BetterRest")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar
+            {
+                Button("Calculate", action: calculateBedtime)
+            }
         }
-        .padding()
+    }
+    
+    func calculateBedtime() {
+        
     }
 }
 
 #Preview {
     ContentView()
+    //SandBoxView()
+}
+
+struct SandBoxView: View {
+    @State private var wakeUp = Date.now
+    
+    var body: some View {
+        DatePicker("Please enter a date", selection: $wakeUp)
+            .labelsHidden()
+    }
 }
